11963d671a797cdbc4feebce288d28bce45f72c0
Yuriy Koziy <yukoziy@gmail.com>
2022-05-15 19:51:47 -0400

    Showing proper card images

--- src/components/CardsContainer/Card.js
+++ src/components/CardsContainer/Card.js
@@ -3,8 +3,10 @@ import './CardsContainer.css';
 const Card = (props) => {
   return (
     <div className="card" onClick={props.handleCardClick} data-id={props.id}>
-      <h3>{props.description}</h3>
       <img src={props.img} alt="card..." />
+      <div className="card-overlay">
+        <h3>{props.description}</h3>
+      </div>
     </div>
   );
 };

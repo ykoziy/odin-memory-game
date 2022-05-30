34db21d288f837842c47397d9455ee91e98b7469
Yuriy Koziy <yukoziy@gmail.com>
2022-05-30 13:18:58 -0400

    Added game instructions

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ import styles from '../style/HeaderScoreboard.module.css';
 const Header = ({ score, bestScore }) => {
   return (
     <header className={styles.head}>
-      <h1>Cars! Memory Game</h1>
+      <div className={styles.logo}>
+        <h1>Cars! Memory Game</h1>
+        <p>
+          Get points by clicking on an image but don't click on any more than
+          once.
+        </p>
+      </div>
       <Scoreboard score={score} bestScore={bestScore} />
     </header>
   );

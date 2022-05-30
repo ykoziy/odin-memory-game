1ab9fd4bc661fc9628b7c6d94e44143b4acd3ebd
Yuriy Koziy <yukoziy@gmail.com>
2022-05-30 12:58:08 -0400

    Fixed images

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,6 +3,18 @@ import { useEffect, useState } from 'react';
 import CardsContainer from './CardsContainer/CardsContainer';
 import GameEnd from './GameEnd';
 import styles from '../style/Game.module.css';
+import fordGTImage from '../assets/images/2004-ford-gt.jpg';
+import fordMustangImage from '../assets/images/1968-ford-mustang-gt.jpg';
+import deloreanImage from '../assets/images/1982-DMC-Delorean.jpg';
+import vwGtiImage from '../assets/images/1985-vw-gti.jpg';
+import abarthImage from '../assets/images/1976-fiat-131-abarth.jpg';
+import astonDB5Image from '../assets/images/1964-aston-martin-db5.jpg';
+import porscheImage from '../assets/images/1993-porsche-carrera-993.jpg';
+import nsxImage from '../assets/images/1993-honda-nsx.jpg';
+import mclarenImage from '../assets/images/1994-mclaren-f1.jpg';
+import lanciaImage from '../assets/images/1993-lancia-dDelta-integrale-evo-ii-front.jpg';
+import lamboImage from '../assets/images/2003-lambo-gallardo.jpg';
+import ferrariImage from '../assets/images/2002-ferrari-enzo-21.jpg';
 
 const Game = ({
   score,
@@ -14,73 +26,73 @@ const Game = ({
   const gameCards = [
     {
       id: uniqid(),
-      img: '/images/2004-ford-gt.jpg',
+      img: fordGTImage,
       description: '2004 Ford GT',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1968-ford-mustang-gt.jpg',
+      img: fordMustangImage,
       description: '1968 Ford Mustang GT',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1982-DMC-Delorean.jpg',
+      img: deloreanImage,
       description: '1982 DMC Delorean',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1985-vw-gti.jpg',
+      img: vwGtiImage,
       description: '1985 VW GTI',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1976-fiat-131-abarth.jpg',
+      img: abarthImage,
       description: '1976 Fiat 131 Abarth',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1964-aston-martin-db5.jpg',
+      img: astonDB5Image,
       description: '1964 Aston Martin DB5',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1993-porsche-carrera-993.jpg',
+      img: porscheImage,
       description: '1993 Porsche 911 Carrera 993',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1993-honda-nsx.jpg',
+      img: nsxImage,
       description: '1993 Honda NSX',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1994-mclaren-f1.jpg',
+      img: mclarenImage,
       description: '1994 McLaren F1',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/2002-ferrari-enzo-21.jpg',
+      img: ferrariImage,
       description: '2002 Ferrari Enzo',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/2003-lambo-gallardo.jpg',
+      img: lamboImage,
       description: 'Lamborghini Gallardo',
       isPicked: false,
     },
     {
       id: uniqid(),
-      img: '/images/1993-lancia-dDelta-integrale-evo-ii-front.jpg',
+      img: lanciaImage,
       description: '1993 Lancia Delta Integrale Evo II',
       isPicked: false,
     },
